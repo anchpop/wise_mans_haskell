a2e30b0d6ac1d05f79f627e11f7d8bbbfa5a6564
Max Orok <morok@mevex.com>
2019-04-08 17:29:46 -0400

    fix spacing on Euro match of convertToDollars fn

--- haskelltests/should_compile/convertToDollars.hs
+++ haskelltests/should_compile/convertToDollars.hs
@@ -3,4 +3,4 @@ data Currency = Dollars Double | Yen Double | Euros Double     deriving (Show)
 convertToDollars :: Currency -> Currency
 convertToDollars (Dollars d) = Dollars d
 convertToDollars (Yen y)     = Dollars (y * 0.0089)
-convertToDollars (Euros e  ) = Dollars (e * 1.14)
+convertToDollars (Euros e)   = Dollars (e * 1.14)
